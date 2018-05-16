27b4383cc1f767dffc2ddb4d4773a8be284a887f
halbeisent <dev@halbeisen.fr>
2018-05-16 16:08:58 +0200

    Exercice 6 Partie 3 PHP Terminé et Corrigé

--- index.php
+++ index.php
@@ -6,14 +6,9 @@
   <title>Exercice 3 Partie 3 PHP</title>
 </head>
 <body>
-  <?php
-  // Je remplis les variables $firstNumber et $secondNumber avant le calcul
-  $firstNumber = 100;
-  $secondNumber = 5;
-  while ($firstNumber >= 10) { //Tant que la valeur de $firstNumber est supérieure ou égale à 10,
-    echo $firstNumber*$secondNumber . '<br />'; //On affiche le résultat du calcul,
-    $firstNumber--; //Puis on décrémente la variable
-  }
-  ?>
+    <?php
+    // Tant que la valeur de $count est supérieure à 0, on affiche le message ci-dessous:
+    for ($count = 20; $count >= 0; $count--) { ?>
+      <p><?= 'C\'est presque bon'; }?></p>
 </body>
 </html>
